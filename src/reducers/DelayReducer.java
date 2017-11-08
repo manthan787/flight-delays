75f21acf5b8c40cb72fd72455e106404ab3822f1
Manthan Thakar <manthant15@gmail.com>
2017-11-07 20:48:46 -0500

    Update delay reducer

--- src/reducers/DelayReducer.java
+++ src/reducers/DelayReducer.java
@@ -1,30 +1,20 @@
 package reducers;
 
-import java.io.IOException;
-
 import org.apache.hadoop.io.FloatWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
-/**
- * @author: Vineet Trivedi
- * Description:
- * Input: [CustomKey(Key), Delays(Values)]
- * Output: [CustomKey(Key), MeanDelay(Value)]
- * Computes the mean delay from the list of delays obtained against a custom key.
- * Writes the custom key and the mean delay to the output.
- */
+import java.io.IOException;
 
 public class DelayReducer extends Reducer<Text, FloatWritable, Text, FloatWritable> {
-    @Override
-    public void reduce(Text key, Iterable<FloatWritable> values, Context context)
-            throws IOException, InterruptedException {
-        float sum = 0f;
-        int count = 1;
-        for (FloatWritable value : values) {
-            sum += value.get();
-            count++;
+
+    public void reduce(Text key, Iterable<FloatWritable> values, Context context) throws IOException, InterruptedException {
+        float totalDelay = 0f;
+        long count = 0;
+        for(FloatWritable d : values) {
+            totalDelay += d.get();
+            count += 1;
         }
-        context.write(key, new FloatWritable(sum / count));
+        context.write(key, new FloatWritable(totalDelay / count));
     }
 }
