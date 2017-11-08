0e4c1e16a06a0444242a9f298f7e0188df4b2ba6
Manthan Thakar <manthant15@gmail.com>
2017-11-07 20:48:38 -0500

    Add delay mapper

---
+++ src/mappers/DelayMapper.java
@@ -0,0 +1,31 @@
+package mappers;
+
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.Mapper;
+import utils.CSVRecord;
+import utils.Parser;
+import java.io.IOException;
+
+/***
+ * @author Manthan Thakar
+ * Description:
+ * Input: [Offset (Key), Record(Value)]
+ * Output: [Offset (Key), CleanDataWritable(Value)]
+ * It passes the record to Parser. If it passes the sanity checks performed by the parser
+ * the information required by the remaining jobs (Delay, Month, Year, AirportId, AirlineId)
+ * is contained in a custom Writable called CleanDataWritable.
+ * The mapper then writes the Offset and the CleanDataWritable to the output.
+ */
+public class DelayMapper extends Mapper<LongWritable, Text, Text, FloatWritable> {
+
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        if(! value.toString().startsWith("YEAR") ) {
+            CSVRecord record = new CSVRecord(value.toString());
+            Parser p = new Parser(record);
+            if (p.sanityCheck()) {
+                Text outKey = new Text(p.getAirline() + "," +p.getAirport() + "," + p.getMonth() + "," + p.getYear());
+                context.write(outKey, new FloatWritable(p.getArrDelNew()));
+            }
+        }
+    }
+}
